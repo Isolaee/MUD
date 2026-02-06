12ef8ff1bdcf56be40dd8a04974e967ed902d6c0
isolaee <eero.isola@gmail.com>
2026-02-06 22:03:47 +0200

    Pre-commit uses python scripts not shell now.

--- scripts/restage-fixes.ps1
+++
@@ -1,9 +0,0 @@
-# Re-stage any files modified by prior pre-commit hooks
-# This should be the LAST hook in .pre-commit-config.yaml
-
-$modified = git diff --name-only
-if ($modified) {
-    git add $modified
-}
-
-exit 0
